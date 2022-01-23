370fe9ea50cb745cf9aad00a4d1058a29f51b5e6
Nachiket Pedgaonkar <93022151+Nachiket008-pg@users.noreply.github.com>
2022-01-23 16:31:10 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,7 @@
+let a = document.getElementById('ctname').value;
+
+fetch("http://universities.hipolabs.com/search?country=india");
+fetch('http://universities.hipolabs.com/search?country=india')
+.then(function fn(){
+
+})
